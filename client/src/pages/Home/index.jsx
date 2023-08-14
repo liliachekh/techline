783fce859ecb9c1aef5e84c8c490cc19c181dc15
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-08-15 00:31:46 +0300

    ADD: create footer jsx, scss

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,16 +1,18 @@
 import AboutUs from "../../components/AboutUs"
 import Header from "../../components/Header"
-import BecomePartner from "../../components/BecomePartner"
+// import BecomePartner from "../../components/BecomePartner"
 // import style from "./Home.module.scss"
 import B2B from "../../components/B2B"
+import Footer from "../../components/Footer"
 
 export function Home () {
     return (
         <>
         <Header/>
-        <BecomePartner/>
+        {/* <BecomePartner/> */}
         <AboutUs/>
         <B2B/>
+        <Footer/>
         </>
     )
 }
