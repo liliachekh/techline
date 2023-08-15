b23b770cfa34e58ffca52aac99cff27518700ecd
Liliia Chekh <liliacheh@gmail.com>
2023-08-15 11:53:13 +0200

    add header animation, format code with prettier

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import MobiNav from "../MobiNav"
-import { Logo } from "../icons"
-import style from "./header.module.scss"
-import { Link } from "react-router-dom"
+import MobiNav from "../MobiNav";
+import { Logo } from "../icons";
+import style from "./header.module.scss";
+import { Link } from "react-router-dom";
 
-export default function Header () {
+export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [scrolled, setScrolled] = useState(false);
   const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -13,57 +13,57 @@ export default function Header () {
     function handleScroll() {
       const currentScrollPos = window.pageYOffset;
       if (prevScrollPos < currentScrollPos && !scrolled) {
-      // if (window.scrollY > 0) {
         setScrolled(true);
-      } 
-      // else {
-        if (prevScrollPos > currentScrollPos && scrolled) {
+      }
+
+      if (prevScrollPos > currentScrollPos && scrolled) {
         setScrolled(false);
       }
       setPrevScrollPos(currentScrollPos);
-    } 
+    }
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [prevScrollPos, scrolled]);
   function toggleBurgerMenu() {
     setIsOpen(!isOpen);
   }
-    return (
-       <>
-        <header className={`${style.header} ${scrolled && style.scrolled}`}>
-<div className={style.wrapper}>
-    <div className={style.container}>
-    <div className={style.section}>
+  return (
+    <>
+      <header className={`${style.header} ${scrolled && style.scrolled}`}>
+        <div className={style.wrapper}>
+          <div className={style.container}>
+            <div className={style.section}>
               <Link to="/">
                 <div className={style.logo}>
-                  <Logo /> 
+                  <Logo />
                   <span className={style.title}>TECHLINES</span>
                 </div>
               </Link>
-              </div>
-              <div className={style.nav__container}>
+            </div>
+            <div className={style.nav__container}>
               <nav className={style.nav}>
                 <ul className={style.list}>
-                    <li className={style.listItem}>Home</li>
-                    <li className={style.listItem}>About us</li>
-                    <li className={style.listItem}>B2B</li>
-                    <li className={style.listItem}>Contacts</li>
-                    <li className={style.listItem}>Sign up</li>
+                  <li className={style.listItem}>Home</li>
+                  <li className={style.listItem}>About us</li>
+                  <li className={style.listItem}>B2B</li>
+                  <li className={style.listItem}>Contacts</li>
+                  <li className={style.listItem}>Sign up</li>
                 </ul>
               </nav>
               <Link to="/b2b/login">
-                    <span className={style.nav_login}>Log in</span>
-                </Link>
-                </div>
-              <MobiNav 
+                <span className={style.nav_login}>Log in</span>
+              </Link>
+            </div>
+            <MobiNav
               isOpen={isOpen}
-              toggleBurgerMenu={() => toggleBurgerMenu()}/>
-    </div>
-</div>
-        </header>
-        </>
-    )
+              toggleBurgerMenu={() => toggleBurgerMenu()}
+            />
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,16 +1,16 @@
-import AboutUs from "../../components/AboutUs"
-import Header from "../../components/Header"
-import BecomePartner from "../../components/BecomePartner"
+import AboutUs from "../../components/AboutUs";
+import Header from "../../components/Header";
+import BecomePartner from "../../components/BecomePartner";
 // import style from "./Home.module.scss"
-import B2B from "../../components/B2B"
+import B2B from "../../components/B2B";
 
-export function Home () {
-    return (
-        <>
-        <Header/>
-        <BecomePartner/>
-        <AboutUs/>
-        <B2B/>
-        </>
-    )
+export function Home() {
+  return (
+    <>
+      <Header />
+      <BecomePartner />
+      <AboutUs />
+      <B2B />
+    </>
+  );
 }
