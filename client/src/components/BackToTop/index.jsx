04b5fa4700ce37128c43f8ca14226de6f64add75
Liliia Chekh <liliacheh@gmail.com>
2023-08-16 10:55:10 +0200

    add scroll button

--- client/src/components/BackToTop/index.jsx
+++ client/src/components/BackToTop/index.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from 'react';
-import { BackToTopIcon } from '../icons';
-import style from "./backToTop.module.scss"
+import { useState, useEffect } from "react";
+import { BackToTopIcon } from "../icons";
+import style from "./backToTop.module.scss";
 
-export default function BackToTop ({ scrollToTop }) {
+export default function BackToTop({ scrollToTop }) {
   const [isVisible, setIsVisible] = useState(false);
 
   const handleScroll = () => {
@@ -19,18 +19,35 @@ export default function BackToTop ({ scrollToTop }) {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
+  useEffect(() => {
+    let timeoutId;
+
+    if (isVisible) {
+      timeoutId = setTimeout(() => {
+        setIsVisible(false);
+      }, 3000);
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [isVisible]);
+
   return (
     isVisible && (
-    <div className={style.btn} onClick={handleScrollToTop}>
-      <BackToTopIcon />
-    </div>)
+      <div
+        className={`${style.btn} ${isVisible ? style.visible : ''}`}
+        onClick={handleScrollToTop}>
+        <BackToTopIcon />
+      </div>
+    )
   );
-};
-
-
+}
