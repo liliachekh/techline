2c806a4b07bb90e6ca00dfd05d6cf35ec784ba49
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-08-24 14:57:07 +0300

    UPDATE: add validation for Login Form

--- client/src/validation/index.js
+++ client/src/validation/index.js
@@ -38,7 +38,9 @@ export const validationSchemaUser = Yup.object({
 
 export const validationSchemaLogin = Yup.object({
   loginOrEmail: Yup.string()
-    .required("Required Field!"),
+    .required("Required Field!")
+    .min(4, 'Must contain at least 4 characters')
+    .matches(/@/, 'Email must contain "@" symbol'),
   password: Yup.string()
     .min(7, 'Must contain at least 7 letters')
     .max(30, 'Can be no more than 30 characters')
