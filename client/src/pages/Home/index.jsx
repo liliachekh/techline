057463fe43ffa0ee7f0b7c18c4600e04290c5954
marcik357 <marcik357@gmail.com>
2023-08-25 21:38:00 +0300

    DEL: del some comments

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -5,7 +5,6 @@ import Footer from "../../components/Footer";
 // import style from "./Home.module.scss"
 import B2B from "../../components/B2B"
 import { useRef } from "react"
-// import { useCallback, useEffect, useRef, useState } from "react"
 import { useInView } from "framer-motion"
 import BackToTop from "../../components/BackToTop";
 import ProductList from "../../components/ProductList";
@@ -20,37 +19,6 @@ export function Home() {
   const aboutInView = useInView(about, { margin: "-50% 0px" });
   const b2bInView = useInView(b2b, { margin: "-50% 0px" });
   const contactsInView = useInView(contacts, { margin: "-50% 0px" });
-
-  // ====================================================================================================================
-  // ====================================================================================================================
-  // ====================================================================================================================
-  // const [products, setProducts] = useState(null);
-
-  // async function fetchData(url, reqBody) {
-  //   try {
-  //     const response = await fetch(url, reqBody);
-  //     if (!response.ok) {
-  //       // handleError(response, 401);
-  //       const error = await response.json();
-  //       throw new Error(error?.loginOrEmail || error?.password || error?.message || error?.email || error);
-  //     }
-  //     return await response.json();
-  //   } catch (error) {
-  //     throw new Error(error?.message);
-  //   }
-  // }
-
-  // const productLoad = useCallback(async () => {
-  //   const products = await fetchData('https://sea-turtle-app-tgzpt.ondigitalocean.app/api/products');
-  //   setProducts(products);
-  // }, [setProducts])
-
-  // useEffect(() => {
-  //   productLoad()
-  // }, [productLoad])
-  // ====================================================================================================================
-  // ====================================================================================================================
-  // ====================================================================================================================
 
   return (
     <>
