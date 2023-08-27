62ae4ee6a0c23aaf29f99db49dfdad2681a74315
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-08-27 16:43:30 +0300

    UPDATE: add changes to style forgotPassword and ResetPassword

--- client/src/pages/ResetPassword/index.jsx
+++ client/src/pages/ResetPassword/index.jsx
@@ -4,19 +4,19 @@ import ForgotPassword from "../../components/ForgotPassword";
 export function ResetPassword() {
 
     return (
-        <div className={style.loginForm}>
-            <div className={style.loginForm__container}>
-                <main className={style.loginForm__main}>
-                    <div className={style.loginForm__logo}>
+        <div className={style.resetForm}>
+            <div className={style.resetForm__container}>
+                <main className={style.resetForm__main}>
+                    <div className={style.resetForm__logo}>
                         <img src="/images/tech.png" alt="techlines logo" />
                     </div>
-                    <div className={style.loginForm__wrapper}>
+                    <div className={style.resetForm__wrapper}>
                         <ForgotPassword
                         //   callback={onSubmitHandler} 
                         />
                     </div>
                 </main>
-                <footer className={style.loginForm__footer}>
+                <footer className={style.resetForm__footer}>
                     <p>2023 © Techline Distribution. All rights reserved.</p>
                 </footer>
             </div>
