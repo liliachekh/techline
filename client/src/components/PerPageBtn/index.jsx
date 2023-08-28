35f878e3535dcaf54b49b7706bed6a33a3c049c1
marcik357 <marcik357@gmail.com>
2023-08-28 13:02:35 +0300

    UPDATE: PerPageBtn rework like other conrol btns

--- client/src/components/PerPageBtn/index.jsx
+++ client/src/components/PerPageBtn/index.jsx
@@ -1,18 +1,16 @@
-import { useSearchParams } from 'react-router-dom';
 import styles from './perPageBtn.module.scss';
 import { scrollToRef } from '../../utils';
+import useQueryString from '../../hooks';
 
 function PerPageBtn({ newPerPage, scrollTo }) {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { sort, perPage, setSearchParams } = useQueryString();
 
-  const sort = searchParams.get('sort');
-  const perPage = searchParams.get('perPage');
   const activeBtn = (Number(perPage) === newPerPage || (!perPage && newPerPage === 10));
 
   async function clickHandler() {
     if (!activeBtn) {
       sort
-        ? setSearchParams({ sort: sort, perPage: newPerPage, startPage: 1 })
+        ? setSearchParams({ sort, perPage: newPerPage, startPage: 1 })
         : setSearchParams({ perPage: newPerPage, startPage: 1 })
     }
     scrollToRef(scrollTo);
--- client/src/hooks/index.jsx
+++ client/src/hooks/index.jsx
@@ -4,7 +4,7 @@ function useQueryString() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const sort = searchParams.get('sort');
-  const perPage = searchParams.get('perPage') || 10;
+  const perPage = searchParams.get('perPage') || 25;
   const page = searchParams.get('startPage') || 1;
 
   return { sort, perPage, page, setSearchParams }
