73870497964452541168487c51698274a4cfb373
Liliia Chekh <liliacheh@gmail.com>
2023-09-03 20:56:49 +0200

    add error and success messages

--- client/src/components/SignUpForm/index.jsx
+++ client/src/components/SignUpForm/index.jsx
@@ -1,91 +1,101 @@
-import { signInFormFields } from './signInFormFields';
-import style from './signUp.module.scss';
-import FormikForm from '../FormikForm';
+import { signInFormFields } from "./signInFormFields";
+import style from "./signUp.module.scss";
+import FormikForm from "../FormikForm";
 import { AnimatePresence, motion } from "framer-motion";
-import { animateFromLeft } from '../../animation';
+import { animateFromLeft } from "../../animation";
 import { useTranslation } from "react-i18next";
-import { validationSchemaUser } from '../../validation';
-// import { fetchData } from '../../utils';
-// import { useDispatch } from 'react-redux';
-// import { setModalType } from '../../redux/actions/modalActions';
-// import { setErrorAction } from '../../redux/actions/errorActions';
-// import { baseUrl } from '../../utils/vars';
-// import { reqPost } from '../../utils/requestBody';
+import { validationSchemaUser } from "../../validation";
+import { useState, useEffect} from "react";
 
-export default function SignUpForm({ callback, refName }) {
+export default function SignUpForm({ refName }) {
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
   const { t } = useTranslation();
-  // const dispatch = useDispatch();
 
-  // async function onSubmitHandler(values) {
-  //   try {
-  //    const response = await fetch('https://storage.techlines.es/api/customers', JSON.stringify(values))
-  //     return await response.json();
-  //   } catch (error) {
-  //  throw new Error(error?.message);
-  // }
-  // }
-  
-  // function onSubmitHandler(values) {
-  //   alert('Success!')
-  //   console.log(values);
-  // }https://storage.techlines.es/api/customers
+  useEffect(() => {
+    if (successMessage) {
+      const timer = setTimeout(() => {
+        setSuccessMessage("");
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [successMessage]);
+
   async function onSubmitHandler(values) {
-    const newValues = {...values, firstName: values.companyName, lastName: values.companyName, login: values.email.split('@')[0].split(".").join("")}
-   console.log(newValues);
+    const newValues = {
+      ...values,
+      firstName: values.companyName,
+      lastName: values.companyName,
+      login: values.email.split("@")[0].split(".").join(""),
+    };
     try {
       const response = await fetch("http://localhost:4000/api/customers", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(newValues)
+        body: JSON.stringify(newValues),
       });
-  
+
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        const errorData = await response.json();
+        setErrorMessage(errorData.message);
+        setSuccessMessage("");
+      } else {
+        setErrorMessage("");
+        setSuccessMessage("Registration successfully completed! Check your mail"); 
       }
-  
-      const savedCustomer = await response.json();
-      console.log(savedCustomer);
-    } catch (err) {
-      console.log(err);
+    } catch (error) {
+      setErrorMessage("Произошла ошибка при отправке запроса.");
+      setSuccessMessage(""); 
     }
   }
-  
 
   return (
     <section ref={refName} className={style.signUp}>
       <div className={style.signUp__wrapper}>
         <AnimatePresence>
-        <div className={style.signUp__container}>
-          <motion.h1 {...animateFromLeft(0)} className={style.signUp__title}>{t('signup.title')}</motion.h1>
-            <motion.div {...animateFromLeft(0, 0.2)} >
-            <FormikForm
-              initialValues={{
-                companyName: '',
-                countryName: '',
-                vatNr: '',
-                contactPerson: '',
-                email: '',
-                password: '',
-                telephone: '',
-                companyWebsite: '',
-                interestProducts: '',
-                brands: '',
-                comment: '',
-              }}
-              validationSchema={validationSchemaUser}
-              fields={signInFormFields.map(field => ({
-                ...field,
-                label: t(`signup.${field.label}`),
-                placeholder: field.placeholder ? t(`signup.${field.placeholder}`) : '',
-              }))}
-              callback={onSubmitHandler}
-              submitBtn={t('signup.submitButton')} />
-              </motion.div>
-        </div>
+          <div className={style.signUp__container}>
+            <motion.h1 {...animateFromLeft(0)} className={style.signUp__title}>
+              {t("signup.title")}
+            </motion.h1>
+            <motion.div {...animateFromLeft(0, 0.2)}>
+              <FormikForm
+                initialValues={{
+                  companyName: "",
+                  countryName: "",
+                  vatNr: "",
+                  contactPerson: "",
+                  email: "",
+                  password: "",
+                  telephone: "",
+                  companyWebsite: "",
+                  interestProducts: "",
+                  brands: "",
+                  comment: "",
+                }}
+                validationSchema={validationSchemaUser}
+                fields={signInFormFields.map((field) => ({
+                  ...field,
+                  label: t(`signup.${field.label}`),
+                  placeholder: field.placeholder
+                    ? t(`signup.${field.placeholder}`)
+                    : "",
+                }))}
+                callback={onSubmitHandler}
+                submitBtn={t("signup.submitButton")}
+              />
+            </motion.div>
+            {errorMessage && (
+              <div className={style.signUp__errorMessage}>{errorMessage}</div>
+            )}
+            {successMessage && (
+              <div className={style.signUp__successMessage}>{successMessage}</div>
+            )}
+          </div>
         </AnimatePresence>
       </div>
     </section>
-  )
+  );
 }
