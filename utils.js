1b5113fbdd568503b7d80c093d36bd2821c04e69
marcik357 <marcik357@gmail.com>
2023-10-05 22:17:20 +0300

    FIX: toFixed(2) prices

--- utils.js
+++ utils.js
@@ -11,11 +11,11 @@ function getUploadedImagesPath() {
 function getTierPrice(customer, currentPrice) {
   switch (customer?.tier) {
     case 'vip':
-      return Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.005) * 100) / 100
+      return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.005) * 100) / 100).toFixed(2)
     case 'premium':
-      return Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.01) * 100) / 100
+      return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.01) * 100) / 100).toFixed(2)
     case 'beginner':
-      return Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.02) * 100) / 100
+      return (Math.ceil((Number(currentPrice) + Number(currentPrice) * 0.02) * 100) / 100)?.toFixed(2)
 
     default:
       return currentPrice
