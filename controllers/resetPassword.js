37a7d5c2014d362b17dc5f431513ce5698557355
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-10-11 00:44:02 +0300

    UPDATE: start to write code for sending email, not comleted

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -7,10 +7,10 @@ const bcrypt = require("bcryptjs");
 exports.resetPassword = async (req, res) => {
   try {
     // Получите email из запроса
-    const { email } = req.body.registeredEmail;
+    const email = req.body.registeredEmail;
 
     // Проверьте, существует ли аккаунт с этим email
-    const customer = await Customer.findOne({ email });
+    const customer = await Customer.findOne({ email: email });
 
     if (!customer) {
       return res.status(404).json({ message: "Customer not found" });
@@ -24,16 +24,18 @@ exports.resetPassword = async (req, res) => {
     );
 
     // Сохраните токен и срок его действия в базе данных
-    // customer.resetToken = token;
-    // customer.resetTokenExpiresAt = Date.now() + 3600 * 1000; // 1 час в миллисекундах
-    // await customer.save();
+    customer.resetToken = token;
+    customer.resetTokenExpiresAt = Date.now() + 3600 * 1000; // 1 час в миллисекундах
+    await customer.save();
 
     // Отправьте email с ссылкой на сброс пароля, включая токен
     // Здесь вы можете использовать библиотеку для отправки электронной почты (например, nodemailer)
     // и создать письмо с ссылкой, в которой будет указан этот токен
+    const link = `${clientURL}/password-reset?token=${resetToken}&id=${user._id}`;
+    sendEmail(user.email,"Password Reset Request",{name: user.name,link: link,},"./template/requestResetPassword.handlebars");
 
     // В этом примере, мы просто отправим токен в ответе на запрос
-    res.json({ resetToken: token });
+    // res.json({ resetToken: token });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
