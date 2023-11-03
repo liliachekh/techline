389b4561501bb685b957bff47936688819f5f9e8
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-11-03 23:01:59 +0200

    UPDATE: delete not used code

--- controllers/resetPassword.js
+++ controllers/resetPassword.js
@@ -48,11 +48,9 @@ exports.requestPasswordReset = async (req, res) => {
       res
     );
 
-    // res.status(200).json({ customer, mailResult, resetToken });
     res.status(200).json({ message: "Password reset link sent to your email account" });
 
   } catch (err) {
-    console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
@@ -69,8 +67,7 @@ exports.verifyResetPasswordLink = async (req, res) => {
 			resetToken: req.params.token,
 		});
 		if (!token) return res.status(400).send({ message: "Invalid link" });
-    console.log(token)
-		res.status(200).send("Valid Url");
+		res.status(200).send({message: "Valid Url"});
 	} catch (error) {
 		res.status(500).send({ message: "Internal Server Error" });
 	}
@@ -87,14 +84,12 @@ exports.resetPassword = async (req, res) => {
     if (!customer) {
       return res.status(400).json({ message: "Customer not found" });
     }
-    console.log(customer);
 
     const token = await ResetToken.findOne({
 			customerId: customer._id,
 			resetToken: req.params.token,
 		});
 		if (!token) return res.status(400).send({ message: "Invalid link" });
-    console.log(token)
 
     // Обновить пароль пользователя
     const salt = await bcrypt.genSalt(10);
@@ -103,93 +98,10 @@ exports.resetPassword = async (req, res) => {
 		customer.password = hashPassword;
 		await customer.save();
 		await token.remove();
-    console.log(customer);
-    console.log(token);
 
 		res.status(200).send({ message: "Password reset successfully" });
   } catch (err) {
-    console.error(err);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
 
-
-
-
-// Controller for resetting password using token
-// exports.resetPassword = async (req, res) => {
-//   try {
-//     const resetToken = req.params.token;
-//     const id = req.params.id;
-//     const newPassword = req.body.password;
-
-//     // Проверка, совпадают ли токены
-//     if (resetToken !== req.cookies.resetToken) {
-//       return res.status(400).json({ message: "Invalid token" });
-//     }
-
-//     // Найти пользователя по id
-//     const customer = await Customer.findOne({ _id: id });
-
-//     if (!customer) {
-//       return res.status(404).json({ message: "Customer not found" });
-//     }
-
-//     // Обновить пароль пользователя
-//     bcrypt.genSalt(10, (err, salt) => {
-//       bcrypt.hash(newPassword, salt, (err, hash) => {
-//         if (err) {
-//           return res.status(500).json({ message: "Internal Server Error" });
-//         }
-//         customer.password = hash;
-//         customer.save()
-//           .then(() => res.json({ message: "Password reset successfully" }))
-//           .catch(() => res.status(500).json({ message: "Internal Server Error" }));
-//       });
-//     });
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json({ message: "Internal Server Error" });
-//   }
-// };
-
-
-
-// exports.resetPassword = async (req, res) => {
-//     try {
-//       // Получите токен сброса пароля из параметра маршрута
-//       const resetToken = req.params.token;
-  
-//       // Проверьте, существует ли аккаунт с этим токеном
-//       const customer = await Customer.findOne({ resetToken });
-  
-//       if (!customer) {
-//         return res.status(404).json({ message: "Invalid or expired token" });
-//       }
-  
-//       // Проверьте, не истек ли срок действия токена (например, 1 час)
-//       const currentTime = Date.now();
-//       if (customer.resetTokenExpiresAt < currentTime) {
-//         return res.status(401).json({ message: "Token has expired" });
-//       }
-  
-//       // Хешируйте новый пароль и обновите его в базе данных
-//       const newPassword = req.body.newPassword;
-//       bcrypt.genSalt(10, (err, salt) => {
-//         bcrypt.hash(newPassword, salt, (err, hash) => {
-//           if (err) {
-//             return res.status(500).json({ message: "Internal Server Error" });
-//           }
-//           customer.password = hash;
-//           customer.resetToken = null;
-//           customer.resetTokenExpiresAt = null;
-//           customer.save()
-//             .then(() => res.json({ message: "Password reset successfully" }))
-//             .catch(() => res.status(500).json({ message: "Internal Server Error" }));
-//         });
-//       });
-//     } catch (err) {
-//       console.error(err);
-//       res.status(500).json({ message: "Internal Server Error" });
-//     }
-//   };
--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const partners = require("./routes/partners");
 // const mainRoute = require('./routes/index');
 const { getStaticFilesPath } = require("./utils");
 const mintProducts = require("./routes/mintProducts");
-const resetPassword = require("./routes/resetPassword"); // Новый файл
+const resetPassword = require("./routes/resetPassword");
 
 
 const app = express();
@@ -98,7 +98,7 @@ app.use("/api/shipping-methods", shippingMethods);
 app.use("/api/payment-methods", paymentMethods);
 app.use("/api/partners", partners);
 app.use("/api/mintProducts", mintProducts);
-app.use("/api/password-reset", resetPassword); // Новый маршрут
+app.use("/api/password-reset", resetPassword);
 
 // app.use('/', mainRoute);
 
